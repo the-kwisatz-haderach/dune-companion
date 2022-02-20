b59ac370dc8c2fdd902ec9e320d399d1481dd521
gustaflundstrom <gustaflundstrom90@gmail.com>
2022-02-20 11:08:46 +0100

    Update config.

--- packages/server/src/config.ts
+++ packages/server/src/config.ts
@@ -1,5 +1,5 @@
 export const config = {
   HTTP_PORT: process.env.PORT || 8000,
-  REDIS_URL: process.env.REDIS_URL || 'redis',
+  REDIS_URL: process.env.REDIS_URL || 'redis://redis:6379',
   SESSION_SECRET: 'secrets'
 }
