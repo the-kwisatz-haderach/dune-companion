8541063663075680183f57f401f74ab191992cb4
gustaflundstrom <gustaflundstrom90@gmail.com>
2022-02-20 13:11:11 +0100

    Update usePlayer hook.

--- packages/web/src/dune-react/hooks/usePlayer.ts
+++ packages/web/src/dune-react/hooks/usePlayer.ts
@@ -4,5 +4,16 @@ import { useWebsocketContext } from './useWebsocketContext'
 
 export const usePlayer = (): Player => {
   const { getClientId } = useWebsocketContext()
-  return useSelector((state: Game) => state.players[getClientId()])
+  return useSelector(
+    (state: Game) =>
+      state.players[getClientId()] ?? {
+        id: '',
+        isAdmin: false,
+        faction: null,
+        name: '',
+        spice: 0,
+        treacheryCards: 0,
+        hasCompletedPhase: false
+      }
+  )
 }
